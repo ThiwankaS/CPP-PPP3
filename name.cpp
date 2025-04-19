f0d0ea91cfc5a9365333583b96d73d87c9259f74
Thiwanka Somachandra <thivankas@outlook.com>
2025-04-19 06:47:00 +0300

    read in data to a varibale

---
+++ name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	string first_name;
+	cout << "Please enter your first name (followed by \"enter\") : " << endl;
+	cin >> first_name;
+	cout << "Hello, " << first_name << "!\n";
+	return (0);
+}
